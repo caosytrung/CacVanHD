2ea885603d3ae5e18b176e45fc6741e6678189ef
vantu <vantuduong222@gmail.com>
2017-10-28 21:33:18 +0700

    create login func

---
+++ WebApiJwtAuthDemo/Controllers/RoleController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRestaurant.Models;
+
+namespace MyRestaurant.Controllers
+{
+   
+    [Route("api/[controller]/[action]")]
+    public class RoleController : Controller
+    {
+        private readonly MyRestaurantContext mContext;
+        public RoleController(MyRestaurantContext context)
+        {
+            mContext = context;
+        }
+
+        [HttpPost]
+        [ActionName("add")]
+        public IActionResult AddRole([FromBody] Role role)
+        {
+            if (role == null)
+            {
+                return BadRequest();
+            }
+            if(role.Name == "")
+            {
+                return BadRequest("Role name must be required!");
+            }
+
+            Role tmp = mContext.Role.Where(item => item.Name == role.Name).SingleOrDefault();
+            if (tmp != null)
+            {
+                return new ObjectResult("This role has been existed!");
+            }
+            else
+            {
+                mContext.Role.Add(role);
+                mContext.SaveChanges();
+                return new ObjectResult("Add role successfully!");
+            }
+        }
+        [HttpDelete("{location}")]
+        [ActionName("delete")]
+        public IActionResult Delete(string location)
+        {
+            var table = mContext.Rtable.FirstOrDefault(t => t.LocationTable == location);
+            if (table == null)
+            {
+                return new ObjectResult("Delete Error ,Table in location is not exist!");
+            }
+
+            mContext.Rtable.Remove(table);
+            mContext.SaveChanges();
+            return new ObjectResult("Delete Table Successfully !");
+        }
+        [HttpPut]
+        [ActionName("modify")]
+        public IActionResult ChangePro(long id, [FromBody] Rtable table)
+        {
+            if (table == null)
+            {
+                return BadRequest();
+            }
+
+            var tmp = mContext.Rtable.FirstOrDefault(item => item.Id == table.Id);
+            if (tmp == null)
+            {
+                return new ObjectResult("Table Not Found !");
+            }
+
+            tmp.NumberOfSeat = table.NumberOfSeat;
+            tmp.Available = table.Available;
+            tmp.TypeTable = table.TypeTable;
+            tmp.LocationTable = table.LocationTable;
+            tmp.Thumbnail = table.Thumbnail;
+            mContext.Rtable.Update(tmp);
+            mContext.SaveChanges();
+
+            return new ObjectResult("update Table Successfully !");
+        }
+    }
+}
--- WebApiJwtAuthDemo/Controllers/UserController.cs
+++ WebApiJwtAuthDemo/Controllers/UserController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using MyRestaurant;
 using MyRestaurant.Models;
+using MyRestaurant.Options;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace MyRestuarant.Controllers
@@ -13,10 +20,22 @@ namespace MyRestuarant.Controllers
     public class UserController : Controller
     {
         private readonly MyRestaurantContext mContext;
-        public UserController(MyRestaurantContext context)
+        private readonly JwtIssuerOptions _jwtOptions;
+        private readonly ILogger _logger;
+        private readonly JsonSerializerSettings _serializerSettings;
+
+        public UserController(MyRestaurantContext context, IOptions<JwtIssuerOptions> jwtOptions, ILoggerFactory loggerFactory)
         {
             mContext = context;
+            _jwtOptions = jwtOptions.Value;
+            ThrowIfInvalidOptions(_jwtOptions);
 
+            _logger = loggerFactory.CreateLogger<UserController>();
+
+            _serializerSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented
+            };
         }
         [HttpPost]
         [ActionName("register")]
@@ -67,26 +86,99 @@ namespace MyRestuarant.Controllers
         }
 
         [HttpPost]
+        [AllowAnonymous]
         [ActionName("login")]
-        public IActionResult Login([FromBody] PostLogin user)
+        public async Task<IActionResult> Get([FromForm] ApplicationUser applicationUser)
+        {
+            if(applicationUser.Username == "" )
+            {
+                return BadRequest("Some parameters are missing");
+            }
+            var identity = await GetClaimsIdentity(applicationUser);
+            if (identity == null)
+            {
+                _logger.LogInformation($"Invalid username ({applicationUser.Username}) or password ({applicationUser.Password})");
+                return BadRequest("Username or password invalid");
+            }
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, applicationUser.Username),
+                new Claim(JwtRegisteredClaimNames.Jti, await _jwtOptions.JtiGenerator()),
+                new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(_jwtOptions.IssuedAt).ToString(), ClaimValueTypes.Integer64),
+                identity.FindFirst("DisneyCharacter")
+            };
+
+            // Create the JWT security token and encode it.
+            var jwt = new JwtSecurityToken(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: claims,
+                notBefore: _jwtOptions.NotBefore,
+                expires: _jwtOptions.Expiration,
+                signingCredentials: _jwtOptions.SigningCredentials);
+
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            // Serialize and return the response
+            var response = new
+            {
+                access_token = encodedJwt,
+                expires_in = (int)_jwtOptions.ValidFor.TotalSeconds
+            };
+
+            var json = JsonConvert.SerializeObject(response, _serializerSettings);
+            return new OkObjectResult(json);
+        }
+
+        private static void ThrowIfInvalidOptions(JwtIssuerOptions options)
+        {
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            if (options.ValidFor <= TimeSpan.Zero)
+            {
+                throw new ArgumentException("Must be a non-zero TimeSpan.", nameof(JwtIssuerOptions.ValidFor));
+            }
+
+            if (options.SigningCredentials == null)
+            {
+                throw new ArgumentNullException(nameof(JwtIssuerOptions.SigningCredentials));
+            }
+
+            if (options.JtiGenerator == null)
+            {
+                throw new ArgumentNullException(nameof(JwtIssuerOptions.JtiGenerator));
+            }
+        }
+
+        /// <returns>Date converted to seconds since Unix epoch (Jan 1, 1970, midnight UTC).</returns>
+        private static long ToUnixEpochDate(DateTime date)
+          => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+
+        /// <summary>
+        /// IMAGINE BIG RED WARNING SIGNS HERE!
+        /// You'd want to retrieve claims through your claims provider
+        /// in whatever way suits you, the below is purely for demo purposes!
+        /// </summary>
+        private Task<ClaimsIdentity> GetClaimsIdentity(ApplicationUser user)
         {
             Users u = mContext.Users.Where(item => item.Username ==
             user.Username && item.Password == user.Password).SingleOrDefault();
-            
-
-            if(u != null)
+            if (u != null)
             {
-                return new ObjectResult("Login Success !");
-            }
-            return new ObjectResult("Username or Password inn't correct, Please Try againt !");
+                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(user.Username, "Token"),
+                  new[]
+                  {
+                     new Claim("DisneyCharacter", "IAmMickey")
+                  }));
+            }else
+            {
+                return Task.FromResult<ClaimsIdentity>(null);
+            }         
+           
         }
 
-
-        public class PostLogin
-        {
-            public String Username { get; set; }
-            public String Password { get; set; }
-        }
+       
 
     }
 }
